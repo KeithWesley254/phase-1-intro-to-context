b9f18f68f0d2f2df3686abe0bbf7605c1cafbb21
Keith <keithwesley187@gmail.com>
2022-06-19 23:23:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
 // Your code here
+/*
+tricky. Come back n check this.
+*/
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+function createEmployeeRecord(arrRecords){
+    const eRecord = {
+        firstName: `${arrRecords[0]}`,
+        familyName:`${arrRecords[1]}`,
+        title: `${arrRecords[2]}`,
+        payPerHour: arrRecords[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return eRecord
+}
+
+function createEmployeeRecords(arrRecord){
+    const arrRecords = []
+    arrRecord.map(halo => arrRecords.push(createEmployeeRecord(halo)))
+    return arrRecords
+}
+
+function createTimeInEvent(objR, dateIn){
+    const hammerDate = dateIn.split(" ")
+    const date = hammerDate[0]
+    const time = hammerDate[1]
+    const newObj = {
+        type: 'TimeIn',
+        hour: parseInt(time),
+        date: `${date}`
+    }
+    objR.timeInEvents.push(newObj)
+    return objR
+}
+
+function createTimeOutEvent(objR, dateOut){
+    const hammerDate = dateOut.split(" ")
+    const date = hammerDate[0]
+    const time = hammerDate[1]
+    const newObj = {
+        type: 'TimeOut',
+        hour: parseInt(time),
+        date: `${date}`
+    }
+    objR.timeOutEvents.push(newObj)
+    return objR
+}
+
+function hoursWorkedOnDate(objR, date){
+    const timeIn = objR.timeInEvents
+    const hourIn = []
+    timeIn.filter(function(miniObj) {
+        if(miniObj.date === date){
+            hourIn.push(miniObj.hour)
+        }
+    })
+    
+    const timeOut = objR.timeOutEvents
+    const hourOut = []
+    timeOut.filter(function(miniObj){
+        if(miniObj.date === date){
+            hourOut.push(miniObj.hour)
+        }
+    })
+
+    const total = (oldV, newV) => oldV + newV
+
+    const totalHoursIn = hourIn.reduce(total)
+    const totalHoursOut = hourOut.reduce(total)
+    const allTotals = Math.abs(totalHoursIn - totalHoursOut)
+
+    if(allTotals < 1000){
+        const stringified = allTotals.toString().split(0, 1)
+        return parseInt(stringified)
+    }else if(allTotals > 999){
+        const stringified = allTotals.toString()
+        const allHours = stringified.charAt(0) + stringified.charAt(1)
+        return parseInt(allHours)
+    }
+}
+
+function wagesEarnedOnDate(objR, date){
+    const rate = objR.payPerHour
+    const hoursD = hoursWorkedOnDate(objR, date)
+    return rate * hoursD
+}
+
+function allWagesFor(objR){
+    const miniArr = objR.timeInEvents
+    const total = []
+    miniArr.map(lavi => {
+        const inDate = lavi.date
+        total.push(wagesEarnedOnDate(objR, inDate))
+    })
+
+    const reduced = (oldV, newV) => oldV + newV
+    const allWages = total.reduce(reduced)
+    return allWages
+}
+
+function calculatePayroll(objR){
+    const total = []
+    objR.map(obj1 => {
+        const rObj = obj1
+        const allTimeIn = rObj.timeInEvents
+        allTimeIn.map(timeIn => {
+            const inDate = timeIn.date
+            total.push(wagesEarnedOnDate(rObj, inDate))
+        })
+    })
+    const reduced = (oldV, newV) => oldV + newV
+    const allPay = total.reduce(reduced)
+    return  allPay
+}
